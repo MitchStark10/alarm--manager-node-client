dbb8749f7afddeba2304af0bacbc7e589072a7f5
Mitch Stark <MitchStark10@gmail.com>
2021-08-24 20:21:09 -0400

    init project with typescript and base class

---
+++ AMClient.ts
@@ -0,0 +1,16 @@
+export default class AMClient {
+    amUrl: string;
+    apiKey: string;
+
+    // TODO: Create default URL
+    constructor(amUrl: string, apiKey: string) {
+        this.amUrl = amUrl;
+        this.apiKey = apiKey;
+    }
+
+    //TODO: Implement the post logic for a new alert
+
+    //TODO: Implement basic retrieval logic for all ongoing alarms
+
+    //TODO: Implement basic retrieval logic for the most recent alarm
+}
