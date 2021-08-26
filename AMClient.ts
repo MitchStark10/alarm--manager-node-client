cd831e54aec8f5f7cba8b8e2829f6e8795cd4b71
Mitch Stark <MitchStark10@gmail.com>
2021-08-25 21:43:53 -0400

    more work towards http requests, need to figure out how to get around the ts issue

--- AMClient.ts
+++ AMClient.ts
@@ -1,14 +1,49 @@
+const REQUEST_HEADERS = new Headers();
+REQUEST_HEADERS.set('Content-Type', 'application/json');
+
 export default class AMClient {
-    amUrl: string;
+    amHost: string;
     apiKey: string;
+    email: string;
 
     // TODO: Create default URL
-    constructor(amUrl: string, apiKey: string) {
-        this.amUrl = amUrl;
+    constructor(amHost: string, email: string, apiKey: string) {
+        this.amHost = amHost;
+        this.email = email;
         this.apiKey = apiKey;
     }
 
     //TODO: Implement the post logic for a new alert
+
+    postNewAlert(alarmText: string) {
+        const requestBody = {
+            email: this.email,
+            apiKey: this.apiKey,
+            alarmText,
+        };
+
+        return new Promise((resolve, reject) => {
+            fetch({
+                url: this.amHost + '/api/authenticated/alarm/addAlarm',
+                method: 'POST',
+                headers: REQUEST_HEADERS,
+                body: JSON.stringify(requestBody),
+            })
+                .then((response) => {
+                    if (response.ok) {
+                        resolve('Alarm successfully added.');
+                    } else {
+                        //TODO: Test this path
+                        console.error(
+                            'Error occurred preventing the alarm from being added:',
+                            response,
+                        );
+                        reject('Error occurred while adding alarm');
+                    }
+                })
+                .catch((error) => {});
+        });
+    }
 
     //TODO: Implement basic retrieval logic for all ongoing alarms
 
