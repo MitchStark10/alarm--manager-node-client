3dc4f068f0fa96e408b0344ef2425a3dd9d78cae
Mitch Stark <MitchStark10@gmail.com>
2021-11-28 15:34:34 -0500

    fixed second test failure

--- AMClient.test.ts
+++ AMClient.test.ts
@@ -1,37 +1,53 @@
-import AMClient from './AMClient';
+import AMClient from "./AMClient";
 
 const getEnvVarString = (key: string) => {
-    if (process.env[key]) {
-        return process.env[key] || '';
+  if (process.env[key]) {
+    return process.env[key] || "";
+  }
+
+  throw `Environment variable not found: ${key}`;
+};
+
+describe("Test the API client", () => {
+  it("Should connect given correct connection details", () => {
+    const client = new AMClient(
+      getEnvVarString("AM_HOST"),
+      getEnvVarString("EMAIL"),
+      getEnvVarString("API_KEY")
+    );
+    return expect(
+      client.postNewAlert("Automated Test", "Jest-created alarm", true)
+    ).resolves.toContain({
+      success: true,
+    });
+  });
+
+  it("Should throw an error if initialized with empty data", () => {
+    try {
+      new AMClient("", "", "");
+
+      console.error(
+        "An AMClient initialized with empty data should throw an error."
+      );
+      return expect(true).toEqual(false);
+    } catch (error) {
+      // Do nothing
     }
+    return expect(true).toEqual(true);
+  });
 
-    throw `Environment variable not found: ${key}`;
-}
-
-describe('Test the API client', () => {
-    it('Should connect given correct connection details', () => {
-        const client = new AMClient(getEnvVarString('AM_HOST'), getEnvVarString('EMAIL'), getEnvVarString('API_KEY'));
-        return expect(client.postNewAlert('Automated Test', 'Jest-created alarm', true)).resolves.toContain({
-            success: true
-        });
-    });
-
-    it('Should throw an error if initialized with empty data', () => {
-        try {
-            new AMClient('', '', '');
-
-            console.error('An AMClient initialized with empty data should throw an error.');
-            return expect(true).toEqual(false);
-        } catch (error) {
-            // Do nothing
-        }
-        return expect(true).toEqual(true);
-    });
-
-    it('Should throw an error if a request fails', () => {
-        const client = new AMClient('badhost', getEnvVarString('EMAIL'), getEnvVarString('API_KEY'));
-        return expect(client.postNewAlert('Invalid Host', 'This alarm is expected to fail to be created', true)).rejects.toContain({
-            success: false
-        });
-    });
-})
+  it("Should throw an error if a request fails", () => {
+    const client = new AMClient(
+      "http://wwww.badhost.com",
+      getEnvVarString("EMAIL"),
+      getEnvVarString("API_KEY")
+    );
+    return expect(
+      client.postNewAlert(
+        "Invalid Host",
+        "This alarm is expected to fail to be created",
+        true
+      )
+    ).rejects.toEqual("Error occurred while adding alarm");
+  });
+});
--- AMClient.ts
+++ AMClient.ts
@@ -1,82 +1,85 @@
-import fetch, { Headers } from 'node-fetch';
+import fetch, { Headers } from "node-fetch";
 
 const REQUEST_HEADERS = new Headers();
-REQUEST_HEADERS.set('Content-Type', 'application/json');
+REQUEST_HEADERS.set("Content-Type", "application/json");
 
 export default class AMClient {
-    amHost: string;
-    apiKey: string;
-    email: string;
+  amHost: string;
+  apiKey: string;
+  email: string;
 
-    // TODO: Create default URL
-    constructor(amHost: string, email: string, apiKey: string) {
-        this.amHost = amHost;
-        this.email = email;
-        this.apiKey = apiKey;
-    }
+  // TODO: Create default URL
+  constructor(amHost: string, email: string, apiKey: string) {
+    this.amHost = amHost;
+    this.email = email;
+    this.apiKey = apiKey;
+  }
 
-    postNewAlert(alarmText: string, alarmDetails: string, sendEmail: boolean) {
-        const requestBody = {
-            email: this.email,
-            apiKey: this.apiKey,
-            alarmText,
-            alarmDetails,
-            sendEmail
-        };
+  postNewAlert(alarmText: string, alarmDetails: string, sendEmail: boolean) {
+    const requestBody = {
+      email: this.email,
+      apiKey: this.apiKey,
+      alarmText,
+      alarmDetails,
+      sendEmail,
+    };
 
-        const requestOptions = {
-            method: 'POST',
-            headers: REQUEST_HEADERS,
-            body: JSON.stringify(requestBody),
-        };
+    const requestOptions = {
+      method: "POST",
+      headers: REQUEST_HEADERS,
+      body: JSON.stringify(requestBody),
+    };
 
-        return new Promise((resolve, reject) => {
-            fetch(
-                this.amHost + '/api/authenticated/alarm/addAlarm',
-                requestOptions,
-            )
-                .then((response) => {
-                    if (response.ok) {
-                        resolve('Alarm successfully added.');
-                    } else {
-                        //TODO: Test this path
-                        console.error(
-                            'Error occurred preventing the alarm from being added:',
-                            response,
-                        );
-                        reject('Error occurred while adding alarm');
-                    }
-                })
-                .catch((error) => {
-                    console.error('Error caught during alarm creation: ' + JSON.stringify(error));
-                    reject(error)
-                });
+    return new Promise((resolve, reject) => {
+      console.log(
+        "fetching with URL",
+        this.amHost + "/api/authenticated/alarm/createAlarm"
+      );
+      fetch(this.amHost + "/api/authenticated/alarm/addAlarm", requestOptions)
+        .then((response) => {
+          if (response.ok) {
+            resolve("Alarm successfully added.");
+          } else {
+            //TODO: Test this path
+            console.error(
+              "Error occurred preventing the alarm from being added:",
+              response
+            );
+            reject("Error occurred while adding alarm");
+          }
+        })
+        .catch((error) => {
+          console.error(
+            "Error caught during alarm creation: " + JSON.stringify(error)
+          );
+          reject(error);
         });
-    }
+    });
+  }
 
-    //TODO: Implement basic retrieval logic for all ongoing alarms
-    retrieveAllOpenAlerts() {
-        const requestBody = {
-            email: this.email,
-            apiKey: this.apiKey,
-        };
+  //TODO: Implement basic retrieval logic for all ongoing alarms
+  retrieveAllOpenAlerts() {
+    const requestBody = {
+      email: this.email,
+      apiKey: this.apiKey,
+    };
 
-        const requestOptions = {
-            method: 'POST',
-            headers: REQUEST_HEADERS,
-            body: JSON.stringify(requestBody),
-        };
+    const requestOptions = {
+      method: "POST",
+      headers: REQUEST_HEADERS,
+      body: JSON.stringify(requestBody),
+    };
 
-        return new Promise((resolve, reject) => {
-            fetch(
-                this.amHost + '/api/authenticated/alarm/retrieveAlarms',
-                requestOptions,
-            )
-                .then((response) => resolve(response))
-                .catch((error) => reject(error));
-        });
-    }
+    return new Promise((resolve, reject) => {
+      fetch(
+        this.amHost + "/api/authenticated/alarm/retrieveAlarms",
+        requestOptions
+      )
+        .then((response) => resolve(response))
+        .catch((error) => reject(error));
+    });
+  }
 
-    // TODO: Implement basic retrieval logic for the most recent alarm. This service does not
-    // yet exist.
+  // TODO: Implement basic retrieval logic for the most recent alarm. This service does not
+  // yet exist.
 }
